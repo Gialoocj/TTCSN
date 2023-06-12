a0084d8adae1a03d7b372dce3240f9466bbb0d11
Gialoocj <94213267+Gialoocj@users.noreply.github.com>
2023-06-12 23:56:35 +0700

    Update BrandAndBound.java

--- src/algorithm/BrandAndBound.java
+++ src/algorithm/BrandAndBound.java
@@ -25,7 +25,6 @@ class BrandAndBound {
         }
     }
 
-    
     private void solveTSP(TSPProblem tspProblem) {
         // Sử dụng dữ liệu trong tspProblem để giải quyết bài toán TSP
         int n = tspProblem.getVertexNames().length;
@@ -35,8 +34,8 @@ class BrandAndBound {
         int[] currentPath = new int[n];
         boolean[] visited = new boolean[n];
 
-        currentPath[0] = tspProblem.getVertexId(tspProblem.getStartPoint(), tspProblem.getVertexNames());
-        visited[tspProblem.getVertexId(tspProblem.getStartPoint(), tspProblem.getVertexNames())] = true;
+        currentPath[0] = TSPProblem.getVertexId(tspProblem.getStartPoint(), tspProblem.getVertexNames());
+        visited[TSPProblem.getVertexId(tspProblem.getStartPoint(), tspProblem.getVertexNames())] = true;
         tspRecursive(tspProblem, currentPath, visited, 1, 0);
 
         System.out.println("Shortest Path: ");
@@ -46,19 +45,57 @@ class BrandAndBound {
                         tspProblem.getVertexNames()[bestPath[i]] + " -> " + tspProblem.getVertexNames()[bestPath[0]]);
                 System.out.println();
 
-                LocalTime totalTime = tspProblem.getStartTime().plusMinutes(bestDistance);
-
-                LocalTime currentStartTime = totalTime;
-                System.out.println("Shortest Distance: " + currentStartTime);
+                System.out.println("Shortest Distance: " + bestDistance + " minutes");
                 return;
             }
             System.out.print(tspProblem.getVertexNames()[bestPath[i]] + " -> ");
         }
     }
 
+    //hàm đánh giá cận
+    private static int evaluateBound(TSPProblem tspProblem, int[] currentPath, int level, int currentDistance) {
+        int remainingDistance = 0;
+        boolean[] visited = new boolean[tspProblem.getN()];
+        for (int i = level; i < tspProblem.getN(); i++) {
+            visited[currentPath[i]] = true;
+        }
+
+        // Ước lượng khoảng cách tối thiểu từ đỉnh hiện tại đến đích
+        int minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < tspProblem.getN(); i++) {
+            if (!visited[i]) {
+                for (int j = 0; j < tspProblem.getN(); j++) {
+                    if (!visited[j] && tspProblem.getWeightMatrix()[i][j] > 0
+                            && tspProblem.getWeightMatrix()[i][j] < minDistance) {
+                        minDistance = tspProblem.getWeightMatrix()[i][j];
+                    }
+                }
+            }
+        }
+
+        // Kiểm tra xem có tắc đường trên đoạn đường hiện tại hay không
+        for (int i = level - 1; i < tspProblem.getN() - 1; i++) {
+            String segment = tspProblem.getVertexNames()[currentPath[i]]
+                    + tspProblem.getVertexNames()[currentPath[i + 1]];
+            for (TrafficJam trafficJam : tspProblem.getTrafficJams()) {
+                if (trafficJam.getSegment().equals(segment)) {
+                    LocalTime currentStartTime = tspProblem.getStartTime().plusMinutes(currentDistance);
+                    if (currentStartTime.isAfter(trafficJam.getStartTime())
+                            && currentStartTime.isBefore(trafficJam.getEndTime())) {
+                        int estimatedDelay = trafficJam.getDelay();
+                        remainingDistance += estimatedDelay;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return currentDistance + minDistance + remainingDistance;
+    }
+
     private static void tspRecursive(TSPProblem tspProblem, int[] currentPath, boolean[] visited,
             int level, int currentDistance) {
-        if (level == tspProblem.getVertexNames().length) {
+        if (level == tspProblem.getN()) {
             // Kiểm tra xem đường đi hiện tại có ngắn hơn đường đi ngắn nhất hiện tại không
             if (tspProblem.getWeightMatrix()[currentPath[level - 1]][currentPath[0]] > 0
                     && currentDistance
@@ -70,7 +107,7 @@ class BrandAndBound {
         }
 
         // Xét các đỉnh kề của đỉnh hiện tại
-        for (int i = 0; i < tspProblem.getVertexNames().length; i++) {
+        for (int i = 0; i < tspProblem.getN(); i++) {
             if (!visited[i] && tspProblem.getWeightMatrix()[currentPath[level - 1]][i] > 0) {
                 // Kiểm tra xem có tắc đường nào trên đoạn đường hiện tại hay không
                 boolean hasTrafficJam = false;
@@ -84,21 +121,22 @@ class BrandAndBound {
                             currentDistance += trafficJam.getDelay();
                             hasTrafficJam = true;
                             break;
-                        }   
+                        }
                     }
                 }
 
                 if (!hasTrafficJam) {
                     currentPath[level] = i;
                     visited[i] = true;
-                    tspRecursive(tspProblem, currentPath, visited, level + 1,
+                    int bound = evaluateBound(tspProblem, currentPath, level + 1,
                             currentDistance + tspProblem.getWeightMatrix()[currentPath[level - 1]][i]);
+                    if (bound < bestDistance) {
+                        tspRecursive(tspProblem, currentPath, visited, level + 1,
+                                currentDistance + tspProblem.getWeightMatrix()[currentPath[level - 1]][i]);
+                    }
                     visited[i] = false;
                 }
             }
         }
     }
 }
-
-    
-
